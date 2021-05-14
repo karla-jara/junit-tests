e7cc2948740c850139d4965f8d22fa6a78ab9690
Karla Jara <jara.karla.43@gmail.com>
2021-05-14 12:47:25 -0500

    added method for Student class

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,7 +1,38 @@
 import java.util.ArrayList;
 
 public class Student {
-    public long id = 0;
+    public long idNum;
     private String name;
     private ArrayList<Integer> grades;
+
+    public void setIdNameInitiateGrades(String studentName, long id){
+        this.idNum = id;
+        this.name = studentName;
+        this.grades = new ArrayList<>();
+    }
+
+    public long getIdNum(){
+        return idNum;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    public ArrayList<Integer> getGrades(){
+        return grades;
+    }
+
+    public double gerGradeAverage(){
+        double totalPoints = 0;
+        double numberOfGrades = grades.size();
+        for(int grade : grades){
+            totalPoints += grade;
+        }
+        return totalPoints/numberOfGrades;
+    }
 }
