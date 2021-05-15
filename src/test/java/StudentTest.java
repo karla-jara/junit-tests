acdb615ac1d9c2757e7642bfcc772c47e4e09de7
Karla Jara <jara.karla.43@gmail.com>
2021-05-15 18:43:42 -0500

    added first test successfully

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,2 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class StudentTest {
+    @Test
+    public void testIfNameIsEquals(){
+        String expected = "Karla";
+        String actual = "Karla";
+
+        assertEquals(expected,actual);
+
+    }
 }
+
+//@Before
+//public void setUp(){}
